f3acffe39afbe86ff387692ae8ca24cbf3711146
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-01 14:52:43 +0100

    Also automatically download inputs

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
+from datetime import date
 import os
 import re
+
+import requests
 
 import pytest
 
@@ -15,8 +18,23 @@ def gen_fixture(filename):
 
     return { match.groups()[0]: fixture }
 
+def download_inputs():
+    with open('.cookie', 'r') as f:
+        cookie = f.read().strip()
+    today = min(25, (date.today() - date(2017, 11, 30)).days)
+    if not os.path.isdir('input'):
+        os.mkdir('input')
+    for day in range(1, today + 1):
+        if os.path.exists('input/day{0:02d}.txt'.format(day)):
+            continue
+        response = requests.get('https://adventofcode.com/2017/day/{0}/input'.format(day), cookies={'session': cookie})
+        response.raise_for_status()
+        with open('input/day{0:02d}.txt'.format(day), 'w') as f:
+            f.write(response.text)
+
 def generate_fixtures():
     for filename in os.listdir('input'):
         globals().update(gen_fixture(filename))
 
+download_inputs()
 generate_fixtures()
