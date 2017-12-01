bb3ff1d9b1903ed9271a9aaaa3d19388f90a305d
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-01 10:04:46 +0100

    [1B] Inverse Captcha

--- day01.py
+++ day01.py
@@ -2,15 +2,38 @@
 
 from itertools import cycle, islice
 
+# The captcha requires you to review a sequence of digits (your puzzle input)
+# and find the sum of all digits that match the next digit in the list. The
+# list is circular, so the digit after the last digit is the first digit in the
+# list.
+
 def digit_sum(s):
-    numbers = list(map(int, s))
-    return sum(a for (a, b) in zip(numbers, islice(cycle(numbers), 1, None)) if a==b)
+    return sum(int(a) for (a, b) in zip(s, islice(cycle(s), 1, None)) if a==b)
+
+# Now, instead of considering the next digit, it wants you to consider the
+# digit halfway around the circular list. That is, if your list contains 10
+# items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+# matches it. Fortunately, your list has an even number of elements.
+
+def digit_sum_halfway(s):
+    offset = int(len(s)/2)
+    return sum(int(a) for (a, b) in zip(s, islice(cycle(s), offset, None)) if a==b)
 
 def test_1a_ex1(): assert digit_sum('1122') == 3
 def test_1a_ex2(): assert digit_sum('1111') == 4
 def test_1a_ex3(): assert digit_sum('1234') == 0
 def test_1a_ex4(): assert digit_sum('91212129') == 9
 
+def test_1b_ex1(): assert digit_sum_halfway('1212') == 6
+def test_1b_ex2(): assert digit_sum_halfway('1221') == 0
+def test_1b_ex3(): assert digit_sum_halfway('123425') == 4
+def test_1b_ex4(): assert digit_sum_halfway('123123') == 12
+def test_1b_ex4(): assert digit_sum_halfway('12131415') == 4
+
 def test_1a_answer():
     with open('input/day01.txt') as f:
         assert digit_sum(f.read().strip()) == 1136
+
+def test_1b_answer():
+    with open('input/day01.txt') as f:
+        assert digit_sum_halfway(f.read().strip()) == 1092
