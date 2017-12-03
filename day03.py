c609062b67f8f3fd154e1d513c4b8e0d1b545ad3
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-03 09:53:30 +0100

    [3B] Spiral Memory

--- day03.py
+++ day03.py
@@ -24,9 +24,30 @@ def number_spiral_manhattan(n):
     y, x = number_spiral_coord(n)
     return abs(y) + abs(x)
 
+def number_spiral_sums():
+    sums = { (0, 0): 1 }
+    for (y, x) in number_spiral_coords():
+        sums[y, x] = sum(sums.get((y+dy, x+dx), 0)
+                         for dy in [-1, 0, +1]
+                         for dx in [-1, 0, +1])
+        yield sums[y, x]
+
+def number_spiral_sum(n):
+    return next(islice(number_spiral_sums(), n-1, n))
+
+def number_spiral_sum_answer(n):
+    return next(v for v in number_spiral_sums() if v > n)
+
 def test_3a_ex1(): assert number_spiral_manhattan(1) == 0
 def test_3a_ex2(): assert number_spiral_manhattan(12) == 3
 def test_3a_ex3(): assert number_spiral_manhattan(23) == 2
 def test_3a_ex4(): assert number_spiral_manhattan(1024) == 31
 
+def test_3b_ex1(): assert number_spiral_sum(1) == 1
+def test_3b_ex2(): assert number_spiral_sum(2) == 1
+def test_3b_ex3(): assert number_spiral_sum(3) == 2
+def test_3b_ex4(): assert number_spiral_sum(4) == 4
+def test_3b_ex4(): assert number_spiral_sum(5) == 5
+
 def test_3a_answer(day03_number): assert number_spiral_manhattan(day03_number) == 480
+def test_3b_answer(day03_number): assert number_spiral_sum_answer(day03_number) == 349975
