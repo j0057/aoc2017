4fea97fdac71860585a199c3f4b30678fc4bf319
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-12 07:10:54 +0100

    [12] Digital Plumber: total rewrite with NetworkX

--- day12.py
+++ day12.py
@@ -1,36 +1,17 @@
+import networkx as nx
+
 def parse(lines):
-    return [ (int(p[0]), int(q))
-             for p in (line.split(' <-> ') for line in lines)
-             for q in p[1].split(', ') ]
+    g = nx.Graph()
+    g.add_edges_from((int(p[0]), int(q))
+                     for p in (line.split(' <-> ') for line in lines)
+                     for q in p[1].split(', '))
+    return g
 
-def find_group(links, group, result):
-    for (p, q) in links:
-        if p != group:
-            continue
-        if q in result:
-            continue
-        result.add(q)
-        find_group(links, q, result)
+def group_len(g, n):
+    return next(len(s) for s in nx.connected_components(g) if n in s)
 
-def plumber(links, group):
-    result = set()
-    find_group(links, group, result)
-    return len(result)
-
-def find_groups(links):
-    groups = []
-    for (p, q) in links:
-        for g in groups:
-            if p in g:
-                if q not in g:
-                    g.add(q)
-                    find_group(links, q, g)
-                break
-        else:
-            G = {p, q}
-            groups.append(G)
-            find_group(links, q, G)
-    return len(groups)
+def group_count(g):
+    return len(list(nx.connected_components(g)))
 
 EX12 = [
     '0 <-> 2',
@@ -42,9 +23,9 @@ EX12 = [
     '6 <-> 4, 5'
 ]
 
-def test_12a_ex(): assert plumber(parse(EX12), 0) == 6
-def test_12b_ex(): assert find_groups(parse(EX12)) == 2
+def test_12a_ex(): assert group_len(parse(EX12), 0) == 6
+def test_12b_ex(): assert group_count(parse(EX12)) == 2
 
-def test_12a_answer(day12_lines): assert plumber(parse(day12_lines), 0) == 152
-def test_12b_answer(day12_lines): assert find_groups(parse(day12_lines)) == 186
+def test_12a_answer(day12_lines): assert group_len(parse(day12_lines), 0) == 152
+def test_12b_answer(day12_lines): assert group_count(parse(day12_lines)) == 186
 
