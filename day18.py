abaf75c27dd32b7f05cd2d04f73edaefb5d53d93
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-18 23:37:08 +0100

    [18B] Duet: total rewrite and many failures

--- day18.py
+++ day18.py
@@ -1,77 +1,56 @@
 from collections import defaultdict
 
-def parse(program):
-    parser = { name.replace('op_', ''): func for (name, func) in globals().items() if name.startswith('op_') }
-    for instruction in program:
-        i = instruction.split()
-        yield (parser[i[0]], tuple(int(v) if v.isdigit() or (v[0] == '-' and v[1:].isdigit()) else v for v in i[1:]))
-
 def execute(reg, code):
+    get = lambda reg, x: reg[x] if x.isalpha() else int(x)
     while 0 <= reg['ip'] < len(code):
-        (op, args) = code[reg['ip']]
-        op(reg, *args)
+        op, *a = code[reg['ip']]
+        if   op == 'set':   reg[a[0]] =  get(reg, a[1])
+        elif op == 'add':   reg[a[0]] += get(reg, a[1])
+        elif op == 'mul':   reg[a[0]] *= get(reg, a[1])
+        elif op == 'mod':   reg[a[0]] %= get(reg, a[1])
+        elif op == 'jgz':   reg['ip'] += get(reg, a[1])-1 if get(reg, a[0]) > 0 else 0
+        elif op == 'snd':
+                reg['SEND'].append(get(reg, a[0]))
+                reg['C'] += 1
+        elif op == 'rcv' and reg['V'] == 1:
+            if get(reg, a[0]):
+                reg[a[0]] = reg['LAST'] = reg['RECV'].pop(-1)
+                break
+        elif op == 'rcv' and reg['V'] == 2:
+            if reg['RECV']:
+                reg['WAIT'] = 0
+                reg[a[0]] = reg['RECV'].pop(0)
+            else:
+                reg['WAIT'] = 1
+                return
+        else:
+            raise ValueError(code[reg['ip']])
+        reg['ip'] += 1
 
-def run(program):
-    code = list(parse(program))
+def one(program):
+    code = [line.split() for line in program]
     queue = []
-    reg = defaultdict(lambda: 0, S=queue, R=queue, V=1)
+    reg = defaultdict(lambda: 0, SEND=queue, RECV=queue, V=1)
     execute(reg, code)
     return reg['LAST']
 
 def two(program):
-    pass
+    code = [line.split() for line in program]
+    q0, q1 = [[], []]
+    reg = [defaultdict(lambda: 0, C=0, SEND=q0, RECV=q1, p=0, P=0, V=2),
+           defaultdict(lambda: 0, C=0, SEND=q1, RECV=q0, p=1, P=1, V=2)]
+    p = 1
+    while True:
+        p = 1-p
+        execute(reg[p], code)
+        if reg[p]['WAIT'] and reg[1-p]['WAIT'] and not reg[1-p]['RECV']:
+            break
+        if not (0 <= reg[0]['ip'] < len(code)) and not (0 <= reg[1]['ip'] < len(code)):
+            break
+    return reg[1]['C']
 
-def op_set(reg, tgt, src):
-    reg[tgt] = src if isinstance(src, int) else reg[src]
-    reg['ip'] += 1
+def test_18a_ex(): assert one('set a 1|add a 2|mul a a|mod a 5|snd a|set a 0|rcv a|jgz a -1|set a 1|jgz a -2'.split('|')) == 4
+def test_18b_ex(): assert two('snd 1|snd 2|snd p|rcv a|rcv b|rcv c|rcv d'.split('|')) == 3
 
-def op_add(reg, tgt, src):
-    reg[tgt] += src if isinstance(src, int) else reg[src]
-    reg['ip'] += 1
-
-def op_mul(reg, tgt, src):
-    reg[tgt] *= src if isinstance(src, int) else reg[src]
-    reg['ip'] += 1
-
-def op_mod(reg, tgt, src):
-    reg[tgt] %= src if isinstance(src, int) else reg[src]
-    reg['ip'] += 1
-
-def op_snd(reg, src):
-    reg['S'] += [src] if isinstance(src, int) else [reg[src]]
-    reg['ip'] += 1
-
-def op_rcv(reg, tgt):
-    if reg['V'] == 1:
-        value = tgt if isinstance(tgt, int) else reg[tgt]
-        if value:
-            reg[tgt] = reg['LAST'] = reg['R'].pop()
-            reg['ip'] = -1
-        else:
-            reg['ip'] += 1
-    else:
-        0/0
-
-def op_jgz(reg, tgt, src):
-    value = tgt if isinstance(tgt, int) else reg[tgt]
-    reg['ip'] += src if value else 1
-
-def test_18_ops():
-    queue = []
-    reg = defaultdict(lambda: 0, S=queue, R=queue, V=1)
-    op_set(reg, 'a', 1)     ; assert reg['a'] == 1 ; assert reg['ip'] == 1
-    op_add(reg, 'a', 2)     ; assert reg['a'] == 3 ; assert reg['ip'] == 2
-    op_mul(reg, 'a', 'a')   ; assert reg['a'] == 9 ; assert reg['ip'] == 3
-    op_mod(reg, 'a', 5)     ; assert reg['a'] == 4 ; assert reg['ip'] == 4
-    op_snd(reg, 'a')        ; assert reg['R'] ==[4]; assert reg['ip'] == 5
-    op_set(reg, 'a', 0)     ; assert reg['a'] == 0 ; assert reg['ip'] == 6
-    op_rcv(reg, 'a')        ; assert reg['a'] == 0 ; assert reg['ip'] == 7
-    op_jgz(reg, 'a', -1)                           ; assert reg['ip'] == 8
-    op_set(reg, 'a', 1)     ; assert reg['a'] == 1 ; assert reg['ip'] == 9
-    op_jgz(reg, 'a', -2)                           ; assert reg['ip'] == 7
-    op_jgz(reg, 'a', -1)                           ; assert reg['ip'] == 6
-    op_rcv(reg, 'a')        ; assert reg['a'] == 4 ; assert reg['ip'] == -1
-
-def test_18a_ex(): assert run('set a 1|add a 2|mul a a|mod a 5|snd a|set a 0|rcv a|jgz a -1|set a 1|jgz a -2'.split('|')) == 4
-
-def test_18a_answer(day18_lines): assert run(day18_lines) == 8600
+def test_18a_answer(day18_lines): assert one(day18_lines) == 8600
+def test_18b_answer(day18_lines): assert two(day18_lines) == 7239
