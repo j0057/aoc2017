8d1c9cb62fae32b42b0bf7d0b3a6882f5d340bea
Joost Molenaar <j.j.molenaar@gmail.com>
2017-12-23 21:54:23 +0100

    [22] Sporifica Virus: rework to generator

--- day22.py
+++ day22.py
@@ -1,34 +1,33 @@
 from collections import defaultdict
 
 def parse(lines):
-    grid = [['.#'.index(ch) for ch in line] for line in lines]
-    return defaultdict(int, {x+1j*y: v
-                             for (y, row) in enumerate(grid)
-                             for (x, v) in enumerate(row)
-                             if grid[y][x] == 1 }), len(grid), len(grid[0])
+    cell = [['.#'.index(ch) for ch in line] for line in lines]
+    grid = {x+1j*y: v
+            for (y, row) in enumerate(cell)
+            for (x, v) in enumerate(row)
+            if v == 1}
+    w,h = len(cell[0]), len(cell)
+    p = (w//2) + (h//2) * 1j
+    d = -1j
+    return defaultdict(int, grid), p, d
 
 # 0=clean 1=infected 2=weakened 3=flagged
-def iterate(grid, h, w, N, states):
+def infect(grid, p, d, N, states):
     turns = { 0: lambda d:  d * -1j,
               1: lambda d:  d *  1j,
               2: lambda d:  d,
               3: lambda d: -d }
-    p = (w//2) + (h//2) * 1j
-    d = -1j
-    c = 0
     for _ in range(N):
         d = turns[grid[p]](d)
-        if states[grid[p]] == 1:
-            c += 1
         grid[p] = states[grid[p]]
+        yield (p, grid[p])
         p += d
-    return c
 
-def one(grid, h, w, N):
-    return iterate(grid, h, w, N, {0: 1, 1: 0})
+def one(grid, p, d, N):
+    return sum(v==1 for (_, v) in infect(grid, p, d, N, {0: 1, 1: 0}))
 
-def two(grid, h, w, N):
-    return iterate(grid, h, w, N, {0: 2, 2: 1, 1: 3, 3: 0})
+def two(grid, p, d, N):
+    return sum(v==1 for (_, v) in infect(grid, p, d, N, {0:2, 2:1, 1:3, 3:0}))
 
 def test_22a_ex1(): assert one(*parse(['..#', '#..', '...']),     7) == 5
 def test_22a_ex2(): assert one(*parse(['..#', '#..', '...']),    70) == 41
